0a5338a966dea804d871f72baf1b17b73bb33ea1
chloe.ladreyt <chloe.ladreyt@etu.univ-lyon1.fr>
2024-12-01 22:29:53 +0100

    feat : navbar and cyborg theme

--- app.py
+++ app.py
@@ -1,17 +1,34 @@
 import dash
 from dash import Dash, html, dcc
+import dash_bootstrap_components as dbc
 
-app = Dash(__name__, use_pages=True)
+app = Dash(__name__, use_pages=True,
+           external_stylesheets=[dbc.themes.CYBORG])
+
+nav = dbc.Nav([
+        dbc.NavItem(
+            dbc.NavLink(f"{page['name']} - {page["path"]}", 
+                        active=True, 
+                        href=page["relative_path"]))
+                    for page in dash.page_registry.values()
+    ],
+    pills=True,
+    )
+classic = html.Div([
+        html.Div(
+            dcc.Link(f"{page['name']} - {page['path']}", 
+                     href=page["relative_path"])
+        ) for page in dash.page_registry.values()
+    ])
+
 
 app.layout = html.Div([
     html.H1('Chloe - Header pour simplifier la naviation entre page'),
-    html.Div([
-        html.Div(
-            dcc.Link(f"{page['name']} - {page['path']}", href=page["relative_path"])
-        ) for page in dash.page_registry.values()
-    ]),
+    nav,
     dash.page_container
 ])
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
