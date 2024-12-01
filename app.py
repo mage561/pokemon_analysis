ac048f35132d5b68df46e1eb4397afbd6036b1bd
Pierre-Antoine Navarro <pan-navarro@outlook.fr>
2024-12-01 19:57:20 +0100

    mise en place du bootstrap et de la page moves

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
 import dash
 from dash import Dash, html, dcc
 
-app = Dash(__name__, use_pages=True)
+app = dash.Dash(
+    __name__,
+    use_pages=True,
+    external_stylesheets=[
+        "/assets/vendor/fontawesome-free/css/all.min.css",
+        "/assets/css/sb-admin-2.min.css",
+    ],
+    external_scripts=[
+        "/assets/vendor/jquery/jquery.min.js",
+        "/assets/vendor/bootstrap/js/bootstrap.bundle.min.js",
+        "/assets/vendor/jquery-easing/jquery.easing.min.js",
+        "/assets/js/sb-admin-2.min.js",
+    ]
+)
 
 app.layout = html.Div([
     html.H1('Chloe - Header pour simplifier la naviation entre page'),
--- pages/moves.py
+++ pages/moves.py
@@ -1,8 +1,63 @@
 import dash
-from dash import html
+from dash import html, dcc, callback
+from dash.dependencies import Input, Output
+from utils.ft_pitou import ft_load_moves_dataset, ft_moves_dual_acps, ft_moves_tsne
 
+# Enregistrement de la page
 dash.register_page(__name__, path='/moves')
 
+# Layout de la page avec le style SB Admin 2
 layout = html.Div([
-    html.H1('Pitou - pages des tsne etc pour les moves')
-])
+    # Page Heading
+    html.Div([
+        html.H1("Analyse des Moves Pokémon", className="h3 mb-0 text-gray-800")
+    ], className="d-sm-flex align-items-center justify-content-between mb-4"),
+
+    # Content Row
+    html.Div([
+        # Scatter Matrix Card
+        html.Div([
+            html.Div([
+                # Card Header
+                html.Div([
+                    html.H6("Matrice de Dispersion", className="m-0 font-weight-bold text-primary")
+                ], className="card-header py-3"),
+                # Card Body
+                html.Div([
+                    dcc.Graph(
+                        id='scatter-matrix',
+                        className='chart-area'
+                    )
+                ], className="card-body")
+            ], className="card shadow mb-4")
+        ], className="col-xl-6 col-lg-6"),
+
+        # t-SNE Plot Card
+        html.Div([
+            html.Div([
+                # Card Header
+                html.Div([
+                    html.H6("Analyse t-SNE", className="m-0 font-weight-bold text-primary")
+                ], className="card-header py-3"),
+                # Card Body
+                html.Div([
+                    dcc.Graph(
+                        id='tsne-plot',
+                        className='chart-area'
+                    )
+                ], className="card-body")
+            ], className="card shadow mb-4")
+        ], className="col-xl-6 col-lg-6")
+    ], className="row")
+], className="container-fluid")
+
+@callback(
+    [Output('scatter-matrix', 'figure'),
+     Output('tsne-plot', 'figure')],
+    Input('scatter-matrix', 'id')
+)
+def update_graphs(_):
+    moves_df = ft_load_moves_dataset()
+    scatter_matrix = ft_moves_dual_acps(moves_df)
+    tsne_plot = ft_moves_tsne(moves_df)
+    return scatter_matrix, tsne_plot
