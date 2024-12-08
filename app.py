f4c0d97a31d397b4d8a7e8e335860100099aab63
chloe.ladreyt <chloe.ladreyt@etu.univ-lyon1.fr>
2024-12-08 21:06:10 +0100

    feat : header and footer

--- app.py
+++ app.py
@@ -3,16 +3,19 @@ from dash import Dash, html, dcc
 import dash_bootstrap_components as dbc
 
 app = Dash(__name__, use_pages=True,
-           external_stylesheets=[dbc.themes.CYBORG])
+           external_stylesheets=[dbc.themes.CERULEAN])
 
-nav = dbc.Nav([
+nav = dbc.Nav(
+    class_name="navbar navbar-expand-lg bg-primary", style={'data-bs-theme' : 'dark'},
+    children = [ 
+       
         dbc.NavItem(
-            dbc.NavLink(f"{page['name']} - {page["path"]}", 
+            dbc.Button(f"{page['name']}", 
                         active=True, 
                         href=page["relative_path"]))
                     for page in dash.page_registry.values()
     ],
-    pills=True,
+    pills=False,
     )
 classic = html.Div([
         html.Div(
@@ -22,10 +25,28 @@ classic = html.Div([
     ])
 
 
-app.layout = html.Div([
-    html.H1('Chloe - Header pour simplifier la naviation entre page'),
-    nav,
-    dash.page_container
+
+footer = html.Div(
+    className="footer",
+    children=[
+        html.P("Pokémon Analysis Dash App © Automn 2024"),
+        html.P("Created by Pierre-Antoine NAVARRO and Chloé LADREYT"),
+
+    ],
+    style={
+        'textAlign': 'center',
+        'padding': '1rem',
+        #'backgroundColor': '#007bff',
+        'color': '#007bff'
+    }
+)
+
+app.layout = html.Div(style = {'color': 'dark', "fontSize" : 15},
+    children=[
+        html.Header('Présentation des pokémons et leur caractéristiques'),
+        nav,
+        dash.page_container,
+        footer
 ])
 
 
